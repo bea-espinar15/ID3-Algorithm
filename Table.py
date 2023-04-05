74f1d33b23dc339075d18208e8f47041febda567
StevenMallqui <57865326+StevenMallqui@users.noreply.github.com>
2023-04-05 11:05:09 +0200

    Table

--- Table.py
+++ Table.py
@@ -3,10 +3,19 @@ import tkinter as tk
 
 class Table:
 
-    def __init__(self):
-        atributosJuego = open('input_files/AtributosJuego.txt', 'r')
-        juego = open('input_files/Juego.txt', 'r')
-        titulos = []
-        for line in atributosJuego:
-            word = line.split(',', 1)[0]
-            titulos.append(word)
+    def __init__(self, attributes_input, data_input, table_frame):
+        self.attributes = attributes_input
+        self.data = data_input
+        self.frame = table_frame
+        self.init_GUI()
+
+    def init_GUI(self):
+        table = tk.Listbox(self.frame, width=50, height=10)
+        table.grid(row=0, column=0, padx=10, pady=10)
+        for header in self.attributes:
+            table.insert(tk.END, header)
+
+        for row in self.data:
+            table.insert(tk.END, row)
+
+        table.configure(bg="white", fg="black", font=("console", 12))
